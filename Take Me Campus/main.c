c9afb7686f439d28a29f5f3a0f078cb12dc985fb
simplex3510 <leeyj5>
2020-05-20 20:28:55 +0900

    분업 시작

--- Take Me Campus/main.c
+++ Take Me Campus/main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
-
-int main()
-{
-	printf("Hello World!\n");
-	printf("Hello World KMJ\n");
-	printf("Hello World PSJ\n");
-	printf("Hello World LYS\n");
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+//	printf("Hello World!\n");
+//	printf("Hello World KMJ\n");
+//	printf("Hello World PSJ\n");
+//	printf("Hello World LYS\n");
+//
+//	return 0;
+//}
