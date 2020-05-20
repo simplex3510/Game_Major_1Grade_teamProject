826ac45f64a59382998e0160bc0a425568e986ac
simplex3510 <leeyj5>
2020-05-20 22:08:33 +0900

    추가 이벤트 발생 필요
    일단 기본 화면 출력

--- Take Me Campus/Client.c
+++ Take Me Campus/Client.c
@@ -1,1 +1,16 @@
-#include
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	system("cls");
+	system("title Take Me Campus");
+	system("mode con cols=122 lines=40");
+	system("color F0");
+
+
+	stage1();
+
+
+	return 0;
+}
