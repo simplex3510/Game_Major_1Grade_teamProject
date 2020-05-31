7fa4f133411b1a11953327a0bd98642704120b65
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2020-05-31 22:58:11 +0900

    FMOD 재업로드

---
+++ Take Me Campus/SoundManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//배경음 파일 추가 함수
+void bgmSoundFileAdd(char file[30], int number);
+
+//효과음 파일 추가 함수
+void effectSoundFileAdd(char file[30], int number);
+
+//사운드 기능 준비 함수
+void sound_init();
+
+//사운드 기능 종료 함수
+void sound_release();
+
+//배경음 출력 함수
+void playBgmSound(int number);
+
+//효과음 출력 함수
+void playEffectSound(int number);
+
+//사운드 갱신 - 지속적으로 호출 필요
+void sound_update();
